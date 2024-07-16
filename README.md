1b5b02c20ffe0c626b38946744bfcda058abd9d4
Mike Chambers <mikechambers@gmail.com>
2024-07-15 23:29:36 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Dispatch
+
+Dispatch is a Python 3 script that syncs the current weekly edition of The Economist into reader mode for the Apple Safari browser.
+
+This can be used as an alternative to the Economist iOS app for reading the digital version of the Economist on macOS and iOS. It requires that you have access to the digital version of the Economist.
+
+## Requirements
+
+* Python 3 (has not been tested with Python 2)
+* Safari browser running on macOS
+* Access to the digital version of the Economist
+
+## Usage
+
+### Setup on macOS
+
+On macOS make sure you are logged into the Economist (https://www.economist.com/) in Safari.
+
+If you want the articles to be synced to your machine, and available to read offline, go to Safari > Settings > Advanced and make sure that *Save articles for offline reading automatically* is checked.
+
+Download the dispatch.py script, and place it in a location you will remember.
+
+Run the script with the following command:
+
+```bash
+python3 dispatch.py
+```
+
+This will sync all of the current weeks articles for the Economist into the Reader mode in Safari, which you can view at *Safari > View > Show Reading List Sidebar*
+
+The articles may take a couple of minutes to sync, but you can begin to read them immediately.
+
+### Sync with iOS
+
+If you would like to have the reading list synced with Safari on iOS, enabled iCloud syncing for Safari on iOS and macOS by following the instructions at:
+
+https://support.apple.com/guide/icloud/set-up-safari-mm5400ef10c4/icloud
+
+View this article for information on how to view Reading List in Safari on iOS, including how to enabled offline mode:
+
+https://support.apple.com/en-us/108970
+
+## Questions, Feature Requests, Feedback
+
+If you have any questions, feature requests, need help, or just want to chat, you can ping me on [Twitter](https://twitter.com/mesh) or via email at [mikechambers@gmail.com](mailto:mikechambers@gmail.com).
+
+You can also log bugs and feature requests on the [issues page](https://github.com/mikechambers/firefly-cl/issues).
+
+## License
+
+Project released under a [MIT License](LICENSE.md).
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-orange.svg)](LICENSE.md)
+
---
+++ src/dispatch.py
@@ -0,0 +1,132 @@
+# Copyright (c) 2024 Mike Chambers
+# https://github.com/mikechambers/firefly-cl
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import argparse
+import requests
+import re
+import subprocess
+import sys
+
+BASE_URL = "https://www.economist.com"
+WEEKLY_URL = f"{BASE_URL}/weeklyedition/"
+
+#User agent for the request to the economist
+USER_AGENT = "Dispatch/1.0"
+
+should_print_urls = False
+verbose = False
+
+SECTIONS = [
+    "/the-world-this-week/",
+    "/leaders/",
+    "/letters/",
+    "/by-invitation/",
+    "/briefing/",
+    "/united-states/",
+    "/the-americas/",
+    "/china/",
+    "/middle-east-and-africa/",
+    "/europe/",
+    "/britain/",
+    "/international/",
+    "/special-report/",
+    "/business/",
+    "/finance-and-economics/",
+    "/science-and-technology/",
+    "/culture/",
+    "/economic-and-financial-indicators/s",
+    "/obituary/"
+]
+
+def main():
+    content = load_url_content(WEEKLY_URL)
+
+    if content is None:
+        print("Could not load weekly edition info from the Economist. Aborting")
+        sys.exit(1)
+
+    urls = []
+
+    for section in SECTIONS:
+        pattern = fr'href="({section}[^\"]+)"'
+        found_urls = re.findall(pattern, content)
+        urls.extend(found_urls)
+
+    if should_print_urls:
+        print_urls(urls)
+        sys.exit()
+
+    add_urls_to_reader(urls)
+
+def add_urls_to_reader(urls):
+
+    reversed_urls = urls[::-1]
+
+    for url in reversed_urls:
+
+        u = f"{BASE_URL}{url}"
+
+        script = f'tell application "Safari" to add reading list item "{u}"'
+
+        if verbose:
+            print(script)
+
+        try:
+            subprocess.run(["osascript", "-e", script], check=True)
+        except Exception as e:
+            print(f"Failed to add url to Reader. Skipping. : {u}")
+
+def print_urls(urls):
+    for url in urls:
+        u = f"{BASE_URL}{url}"
+        print(u)
+
+def load_url_content(url):
+    headers = {
+        'User-Agent': USER_AGENT
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
+
+if __name__ == "__main__":
+    # Initialize the argument parser
+    parser = argparse.ArgumentParser(description="Add current weeks articles in The Economist to Safari reader mode.")
+
+    parser.add_argument('--print', dest='print_urls', 
+                        action='store_true', 
+                        help='Runs in print mode and will only print out the urls for current issue.')
+    
+    parser.add_argument('--verbose', dest='verbose', 
+                        action='store_true', 
+                        help='Runs in print mode and will only print out the urls for current issue.')
+
+    #pass in user_agent
+
+    args = parser.parse_args()
+
+    should_print_urls = args.print_urls
+    verbose = args.verbose
+
+    main()
